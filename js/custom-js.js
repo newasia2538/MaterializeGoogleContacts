cf53ee957cb9179544b373ef58e66f0dc6bb4ac2
Maii <mr.kyoya@gmail.com>
2017-06-14 12:31:20 -0700

    make modal for create new contact

--- js/custom-js.js
+++ js/custom-js.js
@@ -5,3 +5,17 @@ $('.button').sideNav({
     draggable: true // Choose whether you can drag to open on touch screens
   }
 );
+
+$('.modal').modal({
+    dismissible: true, // Modal can be dismissed by clicking outside of the modal
+    opacity: .5, // Opacity of modal background
+    inDuration: 300, // Transition in duration
+    outDuration: 200, // Transition out duration
+    startingTop: '4%', // Starting top style attribute
+    endingTop: '10%', // Ending top style attribute
+    ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+
+    },
+    complete: function() {} // Callback for Modal close
+  }
+);
