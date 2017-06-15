8e76a317072361fa7244d3f399e6dcc2c7980b2f
Maii <mr.kyoya@gmail.com>
2017-06-15 15:32:25 +0700

    add header side-nav for all page

--- js/custom-js.js
+++ js/custom-js.js
@@ -20,6 +20,12 @@ $('.modal').modal({
   }
 );
 
+$('.collapsible').collapsible({
+  accordion: false, // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+  onOpen: function(el) {}, // Callback for Collapsible open
+  onClose: function(el) {} // Callback for Collapsible close
+});
+
 $(document).ready(function() {
   $('select').material_select();
 });
